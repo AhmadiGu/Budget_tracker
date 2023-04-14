7d536ac061709ea703d569f799bd18b91ddbfe66
AhmadiGu <ahmadigulalai76@gmail.com>
2023-04-14 13:30:32 +0500

    Modified readme file

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  # Defines the root path route ("/") 
   
   authenticated do
     root to: "categories#index", as: :authenticated_user
   end
-  root to: "home#index", as: :unauthenticated_user
+  root to: "home#index", as: :unauthenticated_user 
+   
    resources :users
    resources :categories
    resources :entities
