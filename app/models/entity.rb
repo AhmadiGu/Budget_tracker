9fc6cbf0754f46a67c02a08efe2d38f3d25b986e
AhmadiGu <ahmadigulalai76@gmail.com>
2023-04-15 10:23:08 +0500

    linters fixed

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -3,6 +3,6 @@ class Entity < ApplicationRecord
   has_and_belongs_to_many :categories
 
   validates :name, presence: true
-  
+
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
